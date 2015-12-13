2ff181bd4dda856724414d467981dd1977483d51
Jacksgong <igzhenjie@gmail.com>
2015-12-14 00:46:06 +0800

    init library-picassoloader for read Loader by Picasso

---
+++ library-picassoloader/src/main/java/com/liulishuo/image7niuloader/util/PicassoImage7NiuLoader.java
@@ -0,0 +1,133 @@
+package com.liulishuo.image7niuloader.util;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.widget.ImageView;
+
+import com.liulishuo.image7niuload.Image7NiuLoader;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Target;
+import com.squareup.picasso.Transformation;
+
+import java.security.InvalidParameterException;
+
+/**
+ * Copyright (c) 2015 LingoChamp Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Created by Jacksgong on 12/13/15.
+ */
+public class PicassoImage7NiuLoader extends Image7NiuLoader<PicassoImage7NiuLoader> {
+
+    static int DEFAULT_PLACE_HOLDER = 0;
+    static int DEFAULT_AVATAR_PLACE_HOLDER = 0;
+
+    static PicassoLoader.TargetProvider DEFAULT_TARGET_PROVIDER = null;
+
+    private boolean isAvatar = false;
+
+    public PicassoImage7NiuLoader(Context context, String oriUrl) {
+        super(context, oriUrl);
+    }
+
+    public PicassoImage7NiuLoader(ImageView imageView, String oriUrl) {
+        super(imageView, oriUrl);
+    }
+
+
+    public PicassoImage7NiuLoader avatar() {
+        this.isAvatar = true;
+        return this;
+    }
+
+
+    @Override
+    public void attachWithNoClear() {
+        if (getImageView() == null) {
+            throw new InvalidParameterException(String.format("imageView must not be null! %s", getOriUrl()));
+        }
+
+        String u = create7NiuUrl();
+
+        Drawable d = getDefaultDrawable();
+
+        if (d == null) {
+            if (isAvatar) {
+                d = getDrawable(getImageView(), DEFAULT_AVATAR_PLACE_HOLDER);
+            } else {
+                d = getDrawable(getImageView(), DEFAULT_PLACE_HOLDER);
+            }
+        }
+
+        PicassoLoader.display(getImageView(), u, d, transformation, findTarget(), attachCallback);
+    }
+
+    private Target findTarget() {
+        if (this.target != null) {
+            return this.target;
+        }
+
+        if (DEFAULT_TARGET_PROVIDER != null) {
+            return DEFAULT_TARGET_PROVIDER.get(getOriUrl(), getContext());
+        }
+
+        return null;
+    }
+
+    private Target target;
+
+    public PicassoImage7NiuLoader target(final Target target) {
+        this.target = target;
+        return this;
+    }
+
+    private Transformation transformation;
+
+    public PicassoImage7NiuLoader transform(final Transformation transformation) {
+        this.transformation = transformation;
+        return this;
+    }
+
+    private PicassoLoader.FetchCallBack fetchCallback;
+
+    public PicassoImage7NiuLoader fetchCallback(final PicassoLoader.FetchCallBack fetchCallback) {
+        this.fetchCallback = fetchCallback;
+        return this;
+    }
+
+    private Callback attachCallback;
+
+    public PicassoImage7NiuLoader attachCallback(final Callback attachCallback) {
+        this.attachCallback = attachCallback;
+        return this;
+    }
+
+    @Override
+    public void fetch() {
+        if (getContext() == null) {
+            throw new InvalidParameterException(String.format("can't get context ?? url[%s]", getOriUrl()));
+        }
+
+        PicassoLoader.fetch(getContext(), create7NiuUrl(), fetchCallback);
+    }
+
+    @Override
+    public void clear() {
+        super.clear();
+        this.attachCallback = null;
+        this.fetchCallback = null;
+        this.transformation = null;
+        this.target = null;
+    }
+}
